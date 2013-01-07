d0038cf16f4e1099d09b3bcd8a026f22a006fc1f
Saphronov Mark <hijarian@gmail.com>
2013-01-07 21:07:50 +0400

    Set up the console application properly and added the cool template for migrations.

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,37 +1,32 @@
 <?php
-
-// This is the configuration for yiic console application.
-// Any writable CConsoleApplication properties can be configured here.
-return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
-
-	// preloading 'log' component
-	'preload'=>array('log'),
-
-	// application components
-	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+/**
+ * This config is in effect when you launch the console application.
+ * Naturally, it differs from the config of web frontend.
+ * However, most settings will be from there.
+ */
+{
+	$main_config = require(dirname(__FILE__).DIRECTORY_SEPARATOR.'main.php');
+	$console_overrides = array(
+		'components'=>array(
+			'log'=>array(
+				'class'=>'CLogRouter',
+				'routes'=>array(
+					array(
+						'class'=>'CFileLogRoute',
+						'levels'=>'error, warning',
+					),
 				),
 			),
 		),
-	),
-);
+
+		'commandMap' => array(
+			'migrate' => array(
+				// For overrides to be in effect, we need to provide the class name
+				'class' => 'system.cli.commands.MigrateCommand',
+				'templateFile' => 'application.migrations.template.template',
+			),
+		),
+	);
+
+	return CMap::mergeArray($main_config, $console_overrides);
+}
--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * Entry point for the console application.
+ * You better do not touch anything here.
+ * Everything you need is in files referenced here
+ *
+ * Basically the only file you probably want to remove from here is 'debugmodeon'
+ * However, as this particular application is about debugging one particular
+ *  Yii extension this decision will probably be not very clever.
+ *
+ * This particular entry point is touched when you launch the ./yiic tool described in the Yii tutorials
+ * It is used here mostly for creating migrations.
+ */
+define('DS', DIRECTORY_SEPARATOR);
 
-// change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../systems/yii/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
+require(__DIR__.DS.'..'.DS.'paths');
+require(__DIR__.DS.'..'.DS.'debugmodeon');
 
+$yiic   = YII_PATH.DS.'yiic.php';
+$config = dirname(__FILE__).DS.'config'.DS.'console.php';
+
+// This file will set up everything well for the console application.
 require_once($yiic);
