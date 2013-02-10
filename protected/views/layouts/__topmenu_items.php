6785f72a618ae7024ec2256ba0007974a18f6b9e
Saphronov Mark <hijarian@gmail.com>
2013-02-10 11:33:16 +0400

    Added the missing menu items for `/gridView` and `/html5Editor` pages.

--- protected/views/layouts/__topmenu_items.php
+++ protected/views/layouts/__topmenu_items.php
@@ -23,4 +23,20 @@ return array(
 		'linkOptions' => array('class' => 'dropdown-toggle', 'data-toggle' => 'dropdown'), 
 		'submenuOptions' => array('class' => 'dropdown-menu')
 	),
+	array(
+		'label' => 'JsonGridView',
+		'url' => array('/jsonGrid/admin')
+	),
+	array(
+		'label' => 'Html5Editor',
+		'url' => '#',
+		'items' => array(
+			array('label' => 'With colors', 'url' => array('/html5Editor/index')),
+			array('label' => 'Without colors', 'url' => array('/html5Editor/noColors')),
+			array('label' => 'Disable default CSS', 'url' => array('/html5Editor/disableDefaultStylesheet')),
+		),
+		'itemOptions' => array('class' => 'dropdown'),
+		'linkOptions' => array('class' => 'dropdown-toggle', 'data-toggle' => 'dropdown'),
+		'submenuOptions' => array('class' => 'dropdown-menu')
+	)
 );
